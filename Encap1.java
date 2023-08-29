6c34a8ad9131b6dfebb68c9eec986c998b2ebe0b
mounika-12-code <84504515+mounika-12-code@users.noreply.github.com>
2023-08-29 16:08:11 +0530

    Add files via upload

---
+++ Encap1.java
@@ -0,0 +1,34 @@
+package operaters;
+
+public class Encap1 {
+	 private String name;
+	 private int  eid;
+	 private int marks;
+	 private String branch;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	
+
+}
---
+++ Encap2.java
@@ -0,0 +1,20 @@
+package operaters;
+
+public class Encap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Encap1 en =new Encap1();
+ en.setBranch("MCA");
+ en.setEid(12);
+ en.setMarks(120);
+ en.setName("mounika");
+ 
+ 
+ System.out.println(en.getBranch());
+ System.out.println(en.getEid());
+ System.out.println(en.getMarks());
+ System.out.println(en.getName());
+	}
+
+}
